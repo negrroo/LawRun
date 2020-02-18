c507681367950daeedbcd6d925c3b6c9c6fc6fa3
Mohammed ELNaggar <mohammedaelnaggar1@gmail.com>
2020-02-18 14:18:04 +0200

    Drivers:GPU: add-adrenoboost

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -58,6 +58,10 @@ static DEFINE_SPINLOCK(suspend_lock);
 
 #define TAG "msm_adreno_tz: "
 
+#if 1
+static unsigned int adrenoboost = 0;
+#endif
+
 static u64 suspend_time;
 static u64 suspend_start;
 static unsigned long acc_total, acc_relative_busy;
