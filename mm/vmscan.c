bc14e174a792b85f2f32fb0fd0ec284f231548d1
Mohammed ELNaggar <mohammedaelnaggar1@gmail.com>
2020-02-18 16:00:39 +0200

    Drivers:Android: add-simple_LMK

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -47,6 +47,7 @@
 #include <linux/prefetch.h>
 #include <linux/printk.h>
 #include <linux/dax.h>
+#include <linux/simple_lmk.h>
 #include <linux/psi.h>
 
 #include <asm/tlbflush.h>
@@ -3326,6 +3327,8 @@ static int balance_pgdat(pg_data_t *pgdat, int order, int classzone_idx)
 
 		sc.reclaim_idx = classzone_idx;
 
+		simple_lmk_decide_reclaim(sc.priority);
+
 		/*
 		 * If the number of buffer_heads exceeds the maximum allowed
 		 * then consider reclaiming from all zones. This has a dual
@@ -3456,6 +3459,7 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 	 * succeed.
 	 */
 	if (prepare_kswapd_sleep(pgdat, reclaim_order, classzone_idx)) {
+		simple_lmk_stop_reclaim();
 		/*
 		 * Compaction records what page blocks it recently failed to
 		 * isolate pages from and skips them in the future scanning.
@@ -3492,6 +3496,7 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 	 */
 	if (!remaining &&
 	    prepare_kswapd_sleep(pgdat, reclaim_order, classzone_idx)) {
+		simple_lmk_stop_reclaim();
 		trace_mm_vmscan_kswapd_sleep(pgdat->node_id);
 
 		/*
