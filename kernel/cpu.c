ad72baada7a87c531dfe77f5fcaecf603df2b120
negrroo <mohammedaelnaggar1@gmail.com>
2020-05-22 15:30:28 +0200

    Cpu: Revert old cpu files     Changes...               Try to fix stuck issue...                     drivers/cpufreq/cpu-boost.c                     drivers/cpufreq/cpufreq.c                     drivers/cpufreq/pasemi-cpufreq.c                     include/linux/cpufreq.h                     kernel/cpu.c

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2108,6 +2108,7 @@ int cpuhp_smt_enable(void)
 
 	cpu_maps_update_begin();
 	cpu_smt_control = CPU_SMT_ENABLED;
+	arch_smt_update();
 	for_each_present_cpu(cpu) {
 		/* Skip online CPUs and CPUs on offline nodes */
 		if (cpu_online(cpu) || !node_online(cpu_to_node(cpu)))
